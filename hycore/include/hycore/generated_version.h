e0bc62c525342fe97ea01de9164c9279a4761e71
BoyeGuillaume <guillaume.boye@epfl.ch>
2025-10-30 20:36:32 +0100

    Add CMake configuration and versioning support
    - Updated CMakeLists.txt to include compile options based on the compiler.
    - Added versioning support with Git integration in GitVersion.cmake.
    - Created version.h.in for auto-generating version information.
    - Implemented hycore library build configuration in hycore/CMakeLists.txt.
    - Introduced exception handling header and source files.
    - Generated version header with versioning details.

---
+++ hycore/include/hycore/generated_version.h
@@ -0,0 +1,20 @@
+#ifndef _HYCORE_VERSION_H
+#define _HYCORE_VERSION_H
+
+/*
+ * This file is auto-generated from cmake/version.h.in by CMake. Do not edit
+ * directly, edit the input file instead.
+ */
+
+#define HY_VERSION_MAJOR 0
+#define HY_VERSION_MINOR 1
+#define HY_VERSION_PATCH 0
+#define HY_VERSION_PRERELEASE "alpha0"
+#define HY_VERSION_COMMIT_SHORT_HASH "bffa4aa"
+#define HY_VERSION_COMMIT_HASH "bffa4aacb158503cd4552096f0dbb9793d01c6ac"
+#define HY_FULL_VERSION "0.1.0-alpha0+bffa4aa"
+#define HY_SIMPLE_VERSION "0.1.0-alpha0"
+
+#define HY_BUILD_SHARED
+
+#endif // _HYCORE_VERSION_H
---
+++ hycore/src/hycore/except.cpp
@@ -0,0 +1,2 @@
+#include <hycore/except.hpp>
+
