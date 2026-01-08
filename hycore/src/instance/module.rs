056261e6e6f02379dc9de323a2ae42bd2d751ff3
BoyeGuillaume <guillaume.boye@epfl.ch>
2026-01-08 21:04:01 +0100

    Add information to function context

--- hycore/src/instance/module.rs
+++ hycore/src/instance/module.rs
@@ -1,6 +1,10 @@
 use std::collections::BTreeMap;
 
-use hyinstr::modules::Module;
+use hyinstr::modules::{
+    InstructionRef, Module,
+    operand::{Label, Name},
+};
+use petgraph::prelude::DiGraphMap;
 use uuid::Uuid;
 
 use crate::specifications::library::SpecificationLibrary;
@@ -9,6 +13,10 @@ use crate::specifications::library::SpecificationLibrary;
 pub struct FunctionContext {
     /// Unique information about this function.
     pub uuid: Uuid,
+    /// The control flow graph of the function.
+    pub cfg: DiGraphMap<Label, Name>,
+    /// The destination map of the function.
+    pub dest_map: BTreeMap<Name, InstructionRef>,
 }
 
 pub struct ModuleContext {
