70f2b05543851e79946f6e24b5922a3cb1f5a46f
BoyeGuillaume <guillaume.boye@epfl.ch>
2026-01-08 18:48:45 +0100

    Add the `ModuleContext` (not yet implemented)

--- hycore/src/instance/module.rs
+++ hycore/src/instance/module.rs
@@ -1,1 +1,10 @@
-pub struct ModuleContext {}
+use hyinstr::modules::Module;
+use uuid::Uuid;
+
+pub struct ModuleContext {
+    /// Unique information about this module.
+    pub uuid: Uuid,
+
+    /// The module itself.
+    pub module: Module,
+}
