1548ffd79b1ed5405a125afb6c8538b2667c32e9
BoyeGuillaume <guillaume.boye@epfl.ch>
2026-02-08 21:14:25 +0100

    Update invalid argc message in sample.c

--- cffi/examples/sample.c
+++ cffi/examples/sample.c
@@ -59,7 +59,7 @@ int main(int argc, char **argv) {
   if (argc < 1)
     return 1;
   if (argc != 2) {
-    printf("Usage: %s <optional_assembly_file>\n", argv[0]);
+    printf("Usage: %s <assembly_file>\n", argv[0]);
     return -1;
   }
 
